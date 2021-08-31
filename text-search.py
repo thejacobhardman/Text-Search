dd0f963ceb2931d8cc75282e9fbad0ea78f919e2
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-08-31 17:51:08 -0600

    Create program

---
+++ text-search.py
@@ -0,0 +1,14 @@
+# Jacob Hardman
+# CS 350
+# Professor
+# 9/1/2021
+
+import tkinter as tk
+
+Window = tk.Tk()
+Window.title("Text Search")
+
+Display = tk.Frame(Window, height="600", width="800")
+Display.pack(expand="true", fill="both")
+
+Window.mainloop()
