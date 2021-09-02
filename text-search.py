c177900760251041f41ed031cca4e8dd79ade3e3
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-09-02 12:31:19 -0600

    finish file importing

--- text-search.py
+++ text-search.py
@@ -7,37 +7,42 @@ import tkinter as tk
 from tkinter import filedialog
 import sys, ctypes
 
+# This code fixes the blurry text that tkinter has when being used on Windows. I got this solution from Stack Overflow:
+# https://stackoverflow.com/questions/36514158/tkinter-output-blurry-for-icon-and-text-python-2-7/43033405
+if __name__ == "__main__":   
+    if 'win' in sys.platform:
+        ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
 def open_file_dialog():
-    path = filedialog.askopenfile(initialdir="/", title="Select File", filetypes=(("txt files", "*.txt"),("all files", "*.*")))
-    
+    path = filedialog.askopenfile(initialdir="/documents", title="Select File", filetypes=(("txt files", "*.txt"),("all files", "*.*")))
+    file_entry.delete(0, "end")
+    file_entry.insert(0, path.name.format())
+    file_contents.delete("1.0", "end")
+    file_contents.insert("1.0", path.read())
+
+# Create main window
 window = tk.Tk()
 window.title("Text Search")
 
+# Master frame to hold two sub frames
 content = tk.Frame(window, height="600", width="800")
 content.pack(expand="true", fill="both")
 
+# Frame that contains the file's text
 text_frame = tk.Frame(content, padx="10", pady="10")
 text_frame.place(relwidth="0.5", relheight="1")
 
 file_contents = tk.Text(text_frame, undo="true")
 file_contents.place(relwidth="1", relheight="1")
 
-options_frame = tk.Frame(content, padx="10", pady="10", bg="blue")
+# Frame that contains the file dialog as well as the search options
+options_frame = tk.Frame(content, padx="10", pady="10")
 options_frame.place(anchor="ne", relx="1", relwidth="0.5", relheight="1")
 
-label = tk.Label(options_frame, text="File Path")
-label.place(relwidth="0.4", relheight="0.05")
+file_button = tk.Button(options_frame, text="Open File", command=open_file_dialog)
+file_button.pack(pady="20")
 
-select_file_button = tk.Button(options_frame, text="Select File", command=open_file_dialog)
-select_file_button.place(anchor="east", relx="0.5", relwidth="0.4", relheight="0.05")
-
-file_path_entry = tk.Entry(options_frame, width="100")
-file_path_entry.place(rely="0.06", relwidth="1", relheight="0.05")
-
-# This code fixes the blurry text that tkinter has when being used on Windows. I got this solution from Stack Overflow:
-# https://stackoverflow.com/questions/36514158/tkinter-output-blurry-for-icon-and-text-python-2-7/43033405
-if __name__ == "__main__":   
-    if 'win' in sys.platform:
-        ctypes.windll.shcore.SetProcessDpiAwareness(1)
+file_entry = tk.Entry(options_frame, width="50")
+file_entry.pack()
 
 window.mainloop()
